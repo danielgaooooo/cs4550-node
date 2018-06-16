268e7d70888aed74c55c592917647c89eb49b6c2
Daniel Gao <danielgaooooo@gmail.com>
2018-06-16 11:14:37 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+var session = require('express-session');
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any-string'
+}));
+
+app.get('/message/:theMessage', function (req, res) {
+    var theMessage = req.params['theMessage'];
+    res.send(theMessage)
+});
+
+
+
+
+app.get('/api/session/set/:name/:value', setSession);
+app.get('/api/session/get/:name', getSession);
+// app.get('/api/session/get', getSessionAll);
+// app.get('/api/session/reset', resetSession);
+
+
+function setSession(req, res) {
+    var name = req.params['name'];
+    var value = req.params['value'];
+    req.session[name] = value;
+    res.send(req.session);
+}
+
+function getSession(req, res) {
+    var name = req.params['name'];
+    var value = req.session[name];
+    res.send(value);
+}
+
+app.listen(3000);
